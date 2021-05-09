1f304bd3af802a614b9b7df500840c31e722aaa9
lab <banana@banana>
2021-05-09 09:20:10 +0100

    POST request to firebase realtiem database END POINT with auth into parameter query

---
+++ raspberry/save_into_realtime_firebase.sh
@@ -0,0 +1,2 @@
+# POST request to firebase realtiem database END POINT with auth into parameter query
+curl -X POST -d '{ "objectIdentifier": "1", "rssi": "NULL" }' https://indoor-location-se-4383a-default-rtdb.europe-west1.firebasedatabase.app/beacon/1/signal.json?auth=tzXOapDFPzel9sQdUBEvXAxtgbLUiYf2LGnKY9xb
